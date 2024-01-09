888dff190ce86e078896b2221253fc9dcf12c45f
Joohyung-Han <65501090+Joohyung-Han@users.noreply.github.com>
2024-01-09 19:38:20 +0900

    add : add lgbm classifying command files

---
+++ scripts/basic_train.sh
@@ -0,0 +1,2 @@
+HYDRA_FULL_ERROR=1 python train.py
+HYDRA_FULL_ERROR=1 python train.py condition=valence
---
+++ scripts/rppg_basic_test.sh
@@ -0,0 +1,2 @@
+HYDRA_FULL_ERROR=1 python test.py --config-name=rppg_basic_test.yaml
+HYDRA_FULL_ERROR=1 python test.py --config-name=rppg_basic_test.yaml condition=valence

cd483801c51409d2120b968832a5eaf7f33f3ed9
Joohyung-Han <65501090+Joohyung-Han@users.noreply.github.com>
2024-01-23 05:19:30 +0900

    update: update args name and tune function

--- src/pipeline/ml_pipeline.py
+++ src/pipeline/ml_pipeline.py
@@ -1,23 +1,26 @@
+from typing import Union
+
 from omegaconf import DictConfig
+from hydra.utils import instantiate
 
 from ..utils.ml_setup import MLSetUp
+from ..tuner_modules.lgbm_tunermodule import LGBMTunerModule
+from ..tuner_modules.xgb_tunermodule import XGBTunerModule
 
 
 def train(config: DictConfig,) -> None:
     ml_setup = MLSetUp(config)
     
-    stimulus_dataset = ml_setup.get_stimulus_dataset()
-    basic_classifier = ml_setup.get_basic_classifier()
+    dataset = ml_setup.get_dataset()
+    classifier = ml_setup.get_architecture_module()
     
-    data, label = stimulus_dataset()
-    basic_classifier.train(
+    data, label = dataset()
+    classifier.train(
         data=data,
         label=label,
         num_folds=config.num_folds,
-        fold_seed=config.fold_seed,
-        boosting_type=config.boosting_type,
-        objective=config.objective,
-        metric=config.metric,
+        seed=config.seed,
+        params_path=config.params_path,
         result_name=config.result_name,
         plt_save_path=config.plt_save_path,
     )
@@ -25,12 +28,23 @@ def train(config: DictConfig,) -> None:
 def test(config: DictConfig,) -> None:
     ml_setup = MLSetUp(config)
     
-    stimulus_dataset = ml_setup.get_stimulus_dataset()
-    basic_classifier = ml_setup.get_basic_classifier()
+    dataset = ml_setup.get_dataset()
+    classifier = ml_setup.get_architecture_module()
     
-    data, label = stimulus_dataset()
-    basic_classifier.test(
+    data, label = dataset()
+    classifier.test(
         data=data,
         label=label,
         result_name=config.result_name,
     )
+
+def tune(config: DictConfig,) -> None:
+    ml_setup = MLSetUp(config)
+    
+    dataset = ml_setup.get_dataset()
+    
+    data, label = dataset()
+    tuner_module: Union[LGBMTunerModule, XGBTunerModule] = instantiate(
+        config.tuner_module, data=data, label=label
+    )
+    tuner_module()

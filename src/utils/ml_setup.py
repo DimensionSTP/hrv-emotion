c3db6e5600e2e1f42426c50e023d5859f509fc4b
Joohyung-Han <65501090+Joohyung-Han@users.noreply.github.com>
2024-01-23 05:18:50 +0900

    update: update setup args name and type hints

--- src/utils/ml_setup.py
+++ src/utils/ml_setup.py
@@ -1,23 +1,22 @@
+from typing import Union
+
 from omegaconf import DictConfig
 from hydra.utils import instantiate
 
 from ..dataset_modules.stimulus_dataset import StimulusDataset
 from ..dataset_modules.survey_dataset import SurveyDataset
-from ..architecture_modules.basic_archimodule import BasicClassifierModule
+from ..architecture_modules.lgbm_archimodule import LGBMClassifierModule
+from ..architecture_modules.xgb_archimodule import XGBClassifierModule
 
 
 class MLSetUp:
     def __init__(self, config: DictConfig,) -> None:
         self.config = config
 
-    def get_stimulus_dataset(self) -> StimulusDataset:
-        stimulus_dataset: StimulusDataset = instantiate(self.config.dataset_module)
-        return stimulus_dataset
+    def get_dataset(self) -> Union[StimulusDataset, SurveyDataset]:
+        dataset: Union[StimulusDataset, SurveyDataset] = instantiate(self.config.dataset_module)
+        return dataset
 
-    def get_survey_dataset(self) -> SurveyDataset:
-        survey_dataset: SurveyDataset = instantiate(self.config.dataset_module)
-        return survey_dataset
-
-    def get_basic_classifier(self) -> BasicClassifierModule:
-        basic_archimodule: BasicClassifierModule = instantiate(self.config.architecture_module)
-        return basic_archimodule
+    def get_architecture_module(self) -> Union[LGBMClassifierModule, XGBClassifierModule]:
+        architecture_module: Union[LGBMClassifierModule, XGBClassifierModule] = instantiate(self.config.architecture_module)
+        return architecture_module
